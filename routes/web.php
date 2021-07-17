3f952bab7c7379d636861003916a961c934c95ef
muratselen <7muratselen@gmail.com>
2021-07-17 17:35:32 +0300

    28.Rota, View, Controller ilişkisi Ön Bilgiler

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-// nasılsın deneme
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/admin', 'Admin\AdminController@index');
