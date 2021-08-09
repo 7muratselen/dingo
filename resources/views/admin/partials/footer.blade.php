5fbf027e3a06f53dc38614e132453b2ce9323a58
muratselen <muratselcuklu@gmail.com>
2021-08-09 17:21:04 +0300

    33.Ders Master parçalamı

---
+++ resources/views/admin/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="sticky-footer bg-white">
+    <div class="container my-auto">
+        <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Dingo Restaurant. 2019 - {{date('d.m.Y')}}</span>
+        </div>
+    </div>
+</footer>
